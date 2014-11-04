608d35439f4eaf42520e4d2b9a585f239c7a4750
Nguyen Xuan Binh <binh@trobz.com>
2014-11-04 10:26:52 +0000

    #8907, Update demo_task and README.md file for booking-chart

--- demo_task/model/task.py
+++ demo_task/model/task.py
@@ -1,63 +1,102 @@
 from openerp.osv import fields
 from openerp.addons.booking_chart.mixin import mixin
-    
+
+
 class task(mixin.resource):
     _inherit = "project.task"
-    
-    # ref to the booking_chart xml_id (if you have created the booking chart manually, you have to override the mixin.resource.get_chart_id method)
-    _booking_chart_ref = 'demo_task.users_booking_chart'
-    
+
+    # ref to the booking_chart xml_id (if you have created the booking chart
+    # manually, you have to override the mixin.resource.get_chart_id method)
+    '''
+        _booking_chart_refs: {}
+        {module_name.booking_chart_xml_id: resource_field_ref}
+        resource_field_ref: is field which link project.task with res.users
+    '''
+    _booking_chart_refs = {
+        'demo_task.users_booking_chart': 'user_id'
+    }
+
+    '''
+        For _booking_resource_map, There are functions can be used
+        Eg: we have
+
+        def _get_message(self, cr, uid, model, context=None):
+            # param: model is a current object's browse record
+            return model and model.note or "It's empty"
+
+        _booking_resource_map = {
+            'message': _get_message,
+        }
+    '''
     _booking_resource_map = {
-        # simple mapping, booking.resource field = task field 
+        # simple mapping, booking.resource field = task field
         'name':        'name',
         'message':     'description',
         'date_start':  'date_start',
         'date_end':    'date_end',
-        # object mapping, booking.resource field = "task.field._name,task.field.id" 
+        # object mapping,
+        # booking.resource field = "task.field._name,task.field.id"
         'resource_ref': 'user_id',
-        # custom mapping, set booking.resource.css_class field when priority is updated with the value of task.booking_css_class
+        'origin_ref':   'user_id',
+        # custom mapping, set booking.resource.css_class field when priority
+        # is updated with the value of task.booking_css_class
         'css_class':   'priority:booking_css_class'
     }
-    
-    
-    def _get_booking_custom_fields(self, cr, uid, ids, field_names, arg, context=None):
+
+    def _get_booking_custom_fields(self, cr, uid, ids, field_names,
+                                   arg, context=None):
         # booking resource color mapping with task.priority
         colors = {
-            '0': 'red', '1': 'orange', '2': 'dark-blue',  '3': 'blue', '4': 'light-blue'
+            '0': 'red',
+            '1': 'orange',
+            '2': 'dark-blue',
+            '3': 'blue',
+            '4': 'light-blue',
         }
         res = {}
-        
+
         for task in self.browse(cr, uid, ids):
-            res[task.id] = colors[task.priority] if task.priority in colors else ""
-        
+            res[task.id] = colors[task.priority] if \
+                task.priority in colors else ""
+
         return res
-    
-    # add a custom field to get the booking class css according to current status
+
+    # add a custom field to get the booking class css
+    # according to current status
     _columns = {
-        'booking_css_class': fields.function(_get_booking_custom_fields, method=True, type='char', string='Booking CSS Class', readonly=True),
+        'booking_css_class': fields.function(_get_booking_custom_fields,
+                                             method=True, type='char',
+                                             string='Booking CSS Class',
+                                             readonly=True),
     }
-    
-    
+
     #
     # FOR DEMO PROPOSE ONLY
     #
-    # This method generate booking.resource from existing task at demo_task module install/update 
+    # This method generate booking.resource
+    # from existing task at demo_task module install/update
     #
     def _generate_resources(self, cr, uid, context=None):
-        
+
         resource = self.pool.get('booking.resource')
-        
-        for task_id in self.search(cr, uid, [('name', '!=', None), ('date_start', '!=', None), ('date_end', '!=', None)], context=context):
-            resource_id = resource.search(cr, uid, [('origin_ref', '=', '%s,%s' % (self._name, task_id))], context=context)
-        
+
+        for task_id in self.search(cr, uid, [('name', '!=', None),
+                                             ('date_start', '!=', None),
+                                             ('date_end', '!=', None)],
+                                   context=context):
+            resource_id = resource.search(cr, uid, [
+                ('origin_ref', '=', '%s,%s' % (self._name, task_id))],
+                context=context)
+
             if not resource_id:
                 tasks = self.browse(cr, uid, [task_id], context=context)
                 task = tasks[0] if len(tasks) > 0 else {}
-                
+
                 mapping = self._map_values(task, task)
                 mapping['origin_ref'] = "%s,%s" % (self._name, task_id)
                 mapping['chart_id'] = self.get_chart_id(cr, uid, task)
-            
-                resource_id = resource.create(cr, uid, mapping, context=context)
-        
+
+                resource_id = resource.create(cr, uid, mapping,
+                                              context=context)
+
 task()
